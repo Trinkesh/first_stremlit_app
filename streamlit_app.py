e63e898ed0bdbe4bdb1ad27d2175b2253e690074
Trinkesh Nimsarkar <62785642+Trinkesh@users.noreply.github.com>
2023-06-19 15:54:01 +0530

    Update streamlit_app.py
    added a text box at the end of the code for jackfruite

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,3 +50,10 @@ my_cur.execute("SELECT * from pc_rivery_db.public.fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruite load list conatins:")
 streamlit.dataframe(my_data_rows) 
+
+
+
+
+fruit_choice = streamlit.text_input('What fruit would you like to add?','Jackfruit')
+streamlit.write('Thanks for adding Jackfruit', fruit_choice)
+
