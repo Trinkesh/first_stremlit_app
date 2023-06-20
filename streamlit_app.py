ec816dd6f04cbcc2fbfa6e0620e27354d33ef028
Trinkesh Nimsarkar <62785642+Trinkesh@users.noreply.github.com>
2023-06-20 15:34:27 +0530

    Update streamlit_app.py
    made some chanages in the code

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,18 +32,21 @@ streamlit.dataframe(fruits_to_show)
 
 # lets add some request libabry to display fruitevice api response
 # lets get the fruite vice data looking little nicer
+
+# new section to display frute vise api reponse 
+
 streamlit.header("Fruityvice Fruit Advice!")
-# add a text entry box and send the input to the fruitevice as a part of the API call
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+	      streamlit.error("Please select a fruit to get the information.")
+  else:
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+        streamlit.dataframe(fruityvice_normalized)
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-# take the json version and normalized it
-
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# genrate theh output as possible
-streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+	streamlit.error()
 
 # don't run anything past here while we troubleshoot 
 
